400949f14f1edb6d84baf44128fd2d8ae0768cf9
mienna91 <43706477+mienna91@users.noreply.github.com>
2020-09-05 23:48:55 +0200

    a whole lot of changes

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ var closeBtn = document.getElementById("closeBtn");
 //displays the modal window
 btn.addEventListener("click", function(event) { 
     modal.style.display = "block";
+    descriptionLabel.textContent = ` (${30} Characters left)`
+    emptyModalInputs();
 });
 
 //closes the modal window, when "X" button is clicked (I know it's not semantically a button)
@@ -23,12 +25,17 @@ closeBtn.addEventListener("click", function(event) {
     modal.style.display = "none";
 });
 
+window.onclick = function(event) {
+    if(event.target == modal)
+        modal.style.display = "none";
+};
+
 //Keeps track of how many characters the user has left in the description input field.
 descriptionCounter.oninput = handleInput;
 
 function handleInput(event) {
     var charCount = 30;
-
+    
     descriptionLabel.textContent = ` (${charCount - event.target.value.length} Characters left)`
 }
 
@@ -54,21 +61,21 @@ function handleSubmission(event) {
         }
     })
 
-    console.log(todoData[0]);
+    modal.style.display = "none";
     renderCardContent(todoData);
 }
 
 
 function renderCardContent (formData) {
-    document.getElementById("todoSection").insertAdjacentHTML("beforeend", template(formData));
+    document.getElementById("todoSection").insertAdjacentHTML("afterbegin", template(formData));
 }
 
 function template(data) {
     return `
     <section class="todoContainer">
         <ul class="todoList">
-            <li class="todoTitle">${data.title}</li>
-            <li class="todoDescription">${data.description}</li>
+            <li class="todoTitle">${data.todoTitle}</li>
+            <li class="todoDescription">${data.todoDescription}</li>
             <li class="todoBtns">
                 <button class="completeBtn">Complete</button>
                 <button class="deleteBtn">Delete</button>
@@ -77,131 +84,8 @@ function template(data) {
     </section> `
 }
 
-//making the template for the card list
-/*function createTemplate() {
-    var cardList = createCardList();
-    return createContainer(cardList);
-}*/
-
-//Creating the cardlist container, appending it to the "todo" section
-//and finally creates the cardlist, appending it to the container.
-/*function createContainer(cardList) {
-    var container = document.createElement("section");
-    styleContainer(container);
-    todos.appendChild(container);
-    container.appendChild(cardList);
-}*/
-
-//function that creates the cardlist and corresponding elements,
-//styling the content and appending it to the cardlist
-/*function createCardList() {
-    var cardList = document.createElement("ul");    
-    var titleItem = document.createElement("li");    
-    var descriptionItem = document.createElement("li");   
-    var btnItem = document.createElement("li");  
-    var completeBtn = document.createElement("button");   
-    var removeBtn = document.createElement("button");
-
-    styleCardListContent(cardList, titleItem, descriptionItem, btnItem, completeBtn, removeBtn);
-    appendCardListContent(cardList, titleItem, descriptionItem, btnItem, completeBtn, removeBtn);
-
-    return cardList;
-}*/
-
-//container styling
-/*function styleContainer(container) {
-    container.style.height = "163px";
-    container.style.width = "282px";
-    container.style.boxShadow = "0px 2px 11px -4px rgba(93,211,216,1)";
-    container.style.minWidth = "282px";
-    container.style.marginRight = "12px";
-    container.style.marginLeft = "12px";
-}*/
-
-//Gathering function for card list styling
-/*function styleCardListContent(list, title, description, btns, completeBtn, deleteBtn) {
-    styleCardList(list);
-    styleTitle(title);
-    styleDescription(description);
-    styleBtnItem(btns);
-    styleCompleteBtn(completeBtn);
-    styleDeleteBtn(deleteBtn);
-}*/
-
-//cardlist styling
-/*function styleCardList(cardList) {
-    cardList.style.listStyleType = "none"
-    cardList.style.fontFamily = "Arial, Helvetica, sans-serif"; 
-    cardList.style.paddingLeft = "25px";
-}*/
-
-//cardlist title styling
-/*function styleTitle(title) {
-    title.innerHTML = "Title";
-    title.style.fontSize = "16px";
-    title.style.fontWeight = "800";
-    title.style.color = "#333333";
-    title.style.marginBottomtitle
-}*/
-
-//cardlist description styling
-/*function styleDescription(description) {
-    description.style.fontSize = "14px";
-    description.style.color = "#333333";
-    description.style.textOverflow = "ellipsis";
-    description.style.whiteSpace = "normal";
-    description.style.overflow = "hidden";
-    description.style.height = "62px";
-    description.style.marginBottom = "10px";
-}*
-
-//completebutton styling
-function styleCompleteBtn(btn) {
-    btn.style.width = "77px";
-    btn.style.height = "23px";
-    btn.style.float = "right";
-    btn.style.marginRight = "20px";
-    btn.style.marginLeft = "5px";
-    btn.style.backgroundColor = "#56D4C2";
-    btn.style.borderRadius = "5px";
-    btn.style.border = "0";
-    btn.style.color = "white";
-    btn.innerHTML = "Complete";
-    btn.style.fontWeight = "600";
-    btn.style.fontSize = "11px";
+function emptyModalInputs() {
+    document.getElementById("todoTitle").value = null;
+    document.getElementById("todoDescription").value = null;
+    document.getElementById("todoAuthor").value = null;
 }
-
-//deletebutton styling
-function styleDeleteBtn(btn) {
-    btn.style.width = "77px";
-    btn.style.height = "23px";
-    btn.style.float = "right";
-    btn.style.backgroundColor = "#EF3340";
-    btn.style.borderRadius = "5px";
-    btn.style.border = "0";
-    btn.style.color = "white";
-    btn.innerHTML = "Delete";
-    btn.style.fontWeight = "600";
-    btn.style.fontSize = "11px";
-}
-
-//append content to card list
-function appendCardListContent(cardList, title, description, btns, completeBtn, deleteBtn) {
-    cardList.appendChild(title);
-    cardList.appendChild(description);
-    cardList.appendChild(btns);
-    btns.appendChild(completeBtn);
-    btns.appendChild(deleteBtn);
-}
-
-//Button list point styling
-function styleBtnItem(btnItem) {
-    btnItem.style.height = "15px";
-}*
-
-
-
-
-
-
-*/
